0db07608664859e7d08f80b4aee6afd954b61dd6
yeguru <yr947987@gmail.com>
2025-02-09 12:53:37 +0530

    .gitigone added

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,16 @@ output "azs_info" {
 output "subnets_info" {
     value = aws_subnet.public
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
